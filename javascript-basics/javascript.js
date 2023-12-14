b22f6f7d059643538427448f897185f37baad745
Cristhian Morales <115376095+moralessaiz@users.noreply.github.com>
2023-12-13 23:15:28 -0500

    Agregando ejercicios de Javascript

---
+++ javascript-basics/javascript.js
@@ -0,0 +1,25 @@
+console.log("Hello,World!")
+
+let user = 'Pérez';
+let age = 25;
+//let message = "Hello!";
+//message = "World!";
+//alert(message);
+
+let hello = 'Hello world!';
+
+let message;
+
+message = hello;
+//alert(hello);
+
+let userName;
+let test123;
+
+let $ = 1;
+let _ = 2;
+
+//alert($ + _)
+
+num = 5;
+alert(num);
