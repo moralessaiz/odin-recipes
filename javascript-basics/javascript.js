85534784cc94111abc8746f37f0d939840f793e7
Cristhian Andres Morales <115376095+moralessaiz@users.noreply.github.com>
2023-12-15 15:42:06 -0500

    commenting previous code

--- javascript-basics/javascript.js
+++ javascript-basics/javascript.js
@@ -1,36 +1,36 @@
-console.log("Hello,World!")
+//console.log("Hello,World!")
 
-let user = 'Pérez';
-let age = 25;
+//let user = 'Pérez';
+//let age = 25;
 //let message = "Hello!";
 //message = "World!";
 //alert(message);
 
-let hello = 'Hello world!';
+//let hello = 'Hello world!';
 
-let message;
+//let message;
 
-message = hello;
+//message = hello;
 //alert(hello);
 
-let userName;
-let test123;
+//let userName;
+//let test123;
 
-let $ = 1;
-let _ = 2;
+/* let $ = 1;
+let _ = 2; */
 
 //alert($ + _)
 
-num = 5;
+//num = 5;
 //alert(num);
 
-let admin;
-let name;
+/* let admin; */
+/* let name; */
 
-name = "John";
-admin = name;
+/* name = "John"; */
+/* admin = name; */
 //alert(admin)
 
-let earthPlanet = "Earth";
-let nameVisitor = "Pedro";
+/* let earthPlanet = "Earth"; */
+/* let nameVisitor = "Pedro"; */
 
